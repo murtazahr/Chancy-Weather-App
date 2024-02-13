1a401a5144e02629bd1faeba55c85a2eb5a2598c
Murtaza Rangwala <murtazahatimr@icloud.com>
2024-02-13 22:55:23 +0530

    Create basic weather app template

---
+++ ChancyWeatherApp/ChancyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChancyWeatherAppApp.swift
+//  ChancyWeatherApp
+//
+//  Created by Murtaza Hatim on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ChancyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChancyWeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,40 @@
+//
+//  WelcomeView.swift
+//  ChancyWeatherApp
+//
+//  Created by Murtaza Hatim on 13/02/24.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to Chancy")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(/*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
